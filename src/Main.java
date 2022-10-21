6daa2ec13c482e4b3bf7188e9d62873687b2ea18
Dan Alexandru <72348740+912-Dan-Alexandru@users.noreply.github.com>
2022-10-21 18:45:54 +0300

    Main func

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import domain.SymbolTable;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	SymbolTable st = new SymbolTable(13);
+    st.add("a");
+    st.add("abc");
+    System.out.println(st.contains("a"));
+    System.out.println(st);
+    st.remove("a");
+    System.out.println(st.contains("a"));
+    st.add("goat");
+    System.out.println(st);
     }
 }
